500e52efaffce2d851f94a0f24dac29cb7267638
Daniel Okello <handsdani@gmail.com>
2021-10-22 11:47:41 +0300

    Created the SpotifyCLient controller

---
+++ SpotifyApi/Controllers/SpotifyController.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using ApiWrapper.SpotifyServiceClient;
+using ApiWrapper.SpotifyServiceClient.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpotifyApi.Controllers
+{
+    public class SpotifyController : Controller
+    {
+        private readonly ISpotifyClient _spotifyClient;
+
+        public SpotifyController(ISpotifyClient spotifyClient)
+        {
+            _spotifyClient = spotifyClient;
+        }
+        // POST
+        [HttpPost("AuthorizationRequest")]
+        public async Task<ActionResult<Response<AuthorizationResponse>>> AuthorizationRequest()
+        {
+            var response = await _spotifyClient.AuthorizationAsync();
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
