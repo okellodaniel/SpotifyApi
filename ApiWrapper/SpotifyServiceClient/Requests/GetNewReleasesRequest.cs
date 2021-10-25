2da21a7a5b0857c0c23637895aaab7536daba754
Daniel Okello <handsdani@gmail.com>
2021-10-25 10:18:36 +0300

    Get New Releases

--- ApiWrapper/SpotifyServiceClient/Requests/GetNewReleasesRequest.cs
+++ ApiWrapper/SpotifyServiceClient/Requests/GetNewReleasesRequest.cs
@@ -2,6 +2,8 @@ namespace ApiWrapper.SpotifyServiceClient.Requests
 {
     public class GetNewReleasesRequest
     {
-        
+        public string Country { get; set; }
+        public int Limit { get; set; }
+        public int Offset { get; set; }
     }
 }
--- ApiWrapper/SpotifyServiceClient/Responses/GetNewReleasesResponse.cs
+++ ApiWrapper/SpotifyServiceClient/Responses/GetNewReleasesResponse.cs
@@ -1,7 +1,25 @@
+using System.Collections.Generic;
+
 namespace ApiWrapper.SpotifyServiceClient.Responses
 {
     public class GetNewReleasesResponse
     {
+        public Albums Albums { get; set; }
+    }
+    public class Item
+    {
         
     }
+
+    public class Albums
+    {
+        public string Href { get; set; }
+        public List<Item> Items { get; set; }
+        public int Limit { get; set; }
+        public string Next { get; set; }
+        public int Offset { get; set; }
+        public string Previous { get; set; }
+        public int Total { get; set; }
+    }
+    
 }
